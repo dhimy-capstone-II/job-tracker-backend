adbcfda9994eb331179b788dd6f03717a47232ec
Dhimy Jean <dhimysoft@gmail.com>
2026-08-02 02:04:39 -0400

    Add user and job application associations

--- models/index.js
+++ models/index.js
@@ -1,7 +1,29 @@
+// models/index.js — one place to collect all models and relationships.
+//
+// Other files can import from here:
+// const { JobApplication, User } = require("../models");
+
 const db = require("../db");
-const JobApplication = require("./JobApplication");
+
+const JobApplication = require("./JobApplication.model");
+const User = require("./user.model");
+
+// ---------- associations ----------
+
+// One user can have many job applications.
+User.hasMany(JobApplication, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+// Each job application belongs to one user.
+// This adds a userId column to job_applications.
+JobApplication.belongsTo(User, {
+  foreignKey: "userId",
+});
 
 module.exports = {
   db,
   JobApplication,
+  User,
 };
